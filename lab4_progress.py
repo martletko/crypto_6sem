124ae4390edd80ffbd2ab430fe60a09831b3a64b
martletko <44828878+martletko@users.noreply.github.com>
2021-03-25 15:39:04 +1000

    Update lab4_progress.py

--- lab4_progress.py
+++ lab4_progress.py
@@ -1,23 +1,21 @@
 ################################# HEX AND BINARY ###############
 
-hexANDbinary = [
-    ['0000', '0'],
-    ['0001', '1'],
-    ['0010', '2'],
-    ['0011', '3'],
-    ['0100', '4'],
-    ['0101', '5'],
-    ['0110', '6'],
-    ['0111', '7'],
-    ['1000', '8'],
-    ['1001', '9'],
-    ['1010', 'a'],
-    ['1011', 'b'],
-    ['1100', 'c'],
-    ['1101', 'd'],
-    ['1110', 'e'],
-    ['1111', 'f']
-]
+hexANDbinary = [['0000', '0'],
+                ['0001', '1'],
+                ['0010', '2'],
+                ['0011', '3'],
+                ['0100', '4'],
+                ['0101', '5'],
+                ['0110', '6'],
+                ['0111', '7'],
+                ['1000', '8'],
+                ['1001', '9'],
+                ['1010', 'a'],
+                ['1011', 'b'],
+                ['1100', 'c'],
+                ['1101', 'd'],
+                ['1110', 'e'],
+                ['1111', 'f']]
 
 b2h = dict()
 for i in range(len(hexANDbinary)):
@@ -50,32 +48,15 @@ def padding(M1b):
     m = m + '1' + M1b
     return m
 
-IV_512 = '0' * 512
-h = IV_512
-N = '0' * 512
-EPSILON = '0' * 512
-
-
-
-# print(h2b == md) # BITCH IT'S THE SAME I AIN'T EVER GONNA USE THIS MANUALLY BULLSHIT BITCH
-
-b = '323130393837363534333231303938373635343332313039383736353433323130393837363534333231303938373635343332313039383736353433323130'
-
-b1 = hex2binary(b)
-b2 = padding(b1) # b2 - дополненное сообщение до 512 бит (в битовом представлении)
-b3 = binary2hex(b2) 
-# print(b3)
-# m_check = '01323130393837363534333231303938373635343332313039383736353433323130393837363534333231303938373635343332313039383736353433323130'
-# print(b3 == m_check)
-
-
 def xor(x, y):
     if len(x) != len(y):
         raise ValueError
     z = ''
     for i in range(len(x)):
-        z += XOR(x[i],y[i])
+        z += str(int(x[i]) ^ int(y[i]))
     return z
+
+# do i really need them?
 
 def XOR(x, y):
     return IF(x, NOT(y), y)
@@ -117,13 +98,17 @@ Pi =[252, 238, 221,  17, 207, 110,  49,  22, 251, 196, 250,
 def S(b):
     hexstring = ''
     parts_8 = [b[x:x+8] for x in range(0, len(b), 8)]
-    for elem in parts_8:
-        t1 = elem
+    for i in range(len(parts_8)):
+        t1 = parts_8[i]
         t2 = int(t1, 2)
         hexelem = hex(Pi[t2])[2:]
+        if len(hexelem) == 1:
+            hexelem = '0' + hexelem
         hexstring += hexelem
+    a1 = len(hexstring) 
     # print(hexstring)
     binstring = hex2binary(hexstring)
+    a2 = len(binstring)
     return binstring
 
 TAU =  [0, 8,  16, 24, 32, 40, 48, 56,
@@ -142,14 +127,6 @@ def P(b):
         result += parts_8[TAU[i]]
     return result
 
-t1 = xor(h, N)
-t2 = S(t1)
-# K= LPS(t1)
-# print(S(t1))
-t3 = P(t2)
-# print(len(t3))
-# print(hex2binary('8e20faa72ba0b470'))
-# 1000111000100000111110101010011100101011101000001011010001110000
 matrixA = [ '8e20faa72ba0b470', '47107ddd9b505a38', 'ad08b0e0c3282d1c', 'd8045870ef14980e', 
             '6c022c38f90a4c07', '3601161cf205268d', '1b8e0b0e798c13c8', '83478b07b2468764',
             'a011d380818e8f40', '5086e740ce47c920', '2843fd2067adea10', '14aff010bdd87508', 
@@ -166,23 +143,6 @@ matrixA = [ '8e20faa72ba0b470', '47107ddd9b505a38', 'ad08b0e0c3282d1c', 'd804587
             '492c024284fbaec0', 'aa16012142f35760', '550b8e9e21f7a530', 'a48b474f9ef5dc18',
             '70a6a56e2440598e', '3853dc371220a247', '1ca76e95091051ad', '0edd37c48a08a6d8', 
             '07e095624504536c', '8d70c431ac02a736', 'c83862965601dd1b', '641c314b2b8ee083']
-
-'''
-t4 = binary2hex(t3)
-#print(t4)
-
-# print(len(t4))
-parts_2 = [t4[x:x+2] for x in range(0, len(t4), 2)]
-# print(parts_8)
-# t5 = hex2binary(parts_8[0])
-# print(t5)
-print(len(parts_2))
-
-
-# s1 = 'fcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfc'
-# print(len(s1)) # 128 длина в 16-ричной системе
-# print(s1 == t4)
-'''
 
 def xor_arr(arr):
     result = hex2binary(arr[0])
@@ -220,16 +180,84 @@ def L(arr):
     # k1 = xor_arr(temp_big[0])
     # print(binary2hex(k1))
     return k1
-t4 = L(t3)
+
+C = ['b1085bda1ecadae9ebcb2f81c0657c1f2f6a76432e45d016714eb88d7585c4fc4b7ce09192676901a2422a08a460d31505767436cc744d23dd806559f2a64507',
+
+    '6fa3b58aa99d2f1a4fe39d460f70b5d7f3feea720a232b9861d55e0f16b501319ab5176b12d699585cb561c2db0aa7ca55dda21bd7cbcd56e679047021b19bb7',
+
+    'f574dcac2bce2fc70a39fc286a3d843506f15e5f529c1f8bf2ea7514b1297b7bd3e20fe490359eb1c1c93a376062db09c2b6f443867adb31991e96f50aba0ab2',
+
+    'ef1fdfb3e81566d2f948e1a05d71e4dd488e857e335c3c7d9d721cad685e353fa9d72c82ed03d675d8b71333935203be3453eaa193e837f1220cbebc84e3d12e',
+
+    '4bea6bacad4747999a3f410c6ca923637f151c1f1686104a359e35d7800fffbdbfcd1747253af5a3dfff00b723271a167a56a27ea9ea63f5601758fd7c6cfe57',
+
+    'ae4faeae1d3ad3d96fa4c33b7a3039c02d66c4f95142a46c187f9ab49af08ec6cffaa6b71c9ab7b40af21f66c2bec6b6bf71c57236904f35fa68407a46647d6e',
+
+    'f4c70e16eeaac5ec51ac86febf240954399ec6c7e6bf87c9d3473e33197a93c90992abc52d822c3706476983284a05043517454ca23c4af38886564d3a14d493',
+
+    '9b1f5b424d93c9a703e7aa020c6e41414eb7f8719c36de1e89b4443b4ddbc49af4892bcb929b069069d18d2bd1a5c42f36acc2355951a8d9a47f0dd4bf02e71e',
+
+    '378f5a541631229b944c9ad8ec165fde3a7d3a1b258942243cd955b7e00d0984800a440bdbb2ceb17b2b8a9aa6079c540e38dc92cb1f2a607261445183235adb',
+
+    'abbedea680056f52382ae548b2e4f3f38941e71cff8a78db1fffe18a1b3361039fe76702af69334b7a1e6c303b7652f43698fad1153bb6c374b4c7fb98459ced',
+
+    '7bcd9ed0efc889fb3002c6cd635afe94d8fa6bbbebab076120018021148466798a1d71efea48b9caefbacd1d7d476e98dea2594ac06fd85d6bcaa4cd81f32d1b',
+
+    '378ee767f11631bad21380b00449b17acda43c32bcdf1d77f82012d430219f9b5d80ef9d1891cc86e71da4aa88e12852faf417d5d9b21b9948bc924af11bd720']
+
+def X(k, a):
+    return xor(k, a)
 
 
+def E(K_0, b2):
+    # k3 = L(P(S(xor(h,N))))
+    K_0 = L(P(S(xor(h,N))))
 
 
-        
+    K = ['0'*12]
+    K[0] = K_0
+
+    for i in range(1, 13, 1):
+        t1 = L(P(S(xor(hex2binary(K[i-1]), hex2binary(C[i-1])))))
+        K.append(t1)
+
+    k1 = K[0]
+    t1 = hex2binary(k1)
+    t2 = b2
+
+    temp = L(P(S(X(t1,t2))))
+    for i in range(1, 12, 1):
+        temp = L(P(S(X(hex2binary(K[i]), hex2binary(temp)))))
+
+    temp = X(hex2binary(K[12]), hex2binary(temp)) # результат функции E в двоичном виде
+    # print(binary2hex(temp))
+    return temp
+
+def g_N(h, b2):
+    t1 = E(L(P(S(xor(h,N)))), b2 )
+    t2 = xor(t1, h)
+    t3 = xor(t2, b2)
+    return t3
 
 
+########################## MAIN #####################################
 
+IV_512 = '0' * 512
+h = IV_512
+N = '0' * 512
+EPSILON = '0' * 512
 
+# print(h2b == md) # BITCH IT'S THE SAME I AIN'T EVER GONNA USE THIS MANUALLY BULLSHIT BITCH
+
+b = '323130393837363534333231303938373635343332313039383736353433323130393837363534333231303938373635343332313039383736353433323130'
+
+b1 = hex2binary(b)
+b2 = padding(b1) # b2 - дополненное сообщение до 512 бит (в битовом представлении)
+b3 = binary2hex(b2) 
+
+h = g_N(h,b2)
+
+N = 
 
 
 
